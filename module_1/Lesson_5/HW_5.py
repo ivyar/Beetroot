a2402189e76d673c3b3f07681136603fe8f19354
Аліна Міснік <alino4ka.m.2015@gmail.com>
2024-12-03 14:06:53 +0200

    add homework 5

--- module_1/Lesson_5/HW_5.py
+++ module_1/Lesson_5/HW_5.py
@@ -1,17 +1,17 @@
 import random
 
-# #Task 1
-# random_num = random.randint(1, 10)
-# user_num = int(input('Введіть число від 1 до 10: '))
-# if random_num == user_num:
-#     print('Вітаємо, ви вгадали!')
-# else:
-#     print(f'Введене число {user_num}, правильне число {random_num}\nВи програли!')
+#Task 1
+random_num = random.randint(1, 10)
+user_num = int(input('Введіть число від 1 до 10: '))
+if random_num == user_num:
+    print('Вітаємо, ви вгадали!')
+else:
+    print(f'Введене число {user_num}, правильне число {random_num}\nВи програли!')
 
-# #Task 2
-# name = input("Введіть ім'я: ")
-# age = int(input('Введіть вік: '))
-# print(f"Hello {name}, on your next birthday you'll be {age + 1} years")
+#Task 2
+name = input("Введіть ім'я: ")
+age = int(input('Введіть вік: '))
+print(f"Hello {name}, on your next birthday you'll be {age + 1} years")
 
 #Task 3
 string = list(input('Your string: '))
